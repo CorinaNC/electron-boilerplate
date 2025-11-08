000489bcb1843fa65967ce901f858fa1d83bad02
CorinaNC <corina0615@gmail.com>
2025-11-08 13:10:47 -0600

    .

---
+++ app/src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <p>This is our layout</p>
+      <ul>
+        <li>
+          <a href="/work">Work</a>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+      </ul>
+      <Outlet /> {/* <Outlet /> is needed to provide nested routes inside of x route */}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ app/src/pages/Work.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div>this is the work page</div>
+  )
+}
+
+export default Work
