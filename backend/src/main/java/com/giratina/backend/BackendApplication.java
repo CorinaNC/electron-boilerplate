2d4997dbe88d7edbeb02d62cb4fe04dddfeb18b4
CorinaNC <corina0615@gmail.com>
2025-11-18 22:24:11 -0600

    it somewhat works?!

--- backend/src/main/java/com/giratina/backend/BackendApplication.java
+++ backend/src/main/java/com/giratina/backend/BackendApplication.java
@@ -4,16 +4,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class BackendApplication implements CommandLineRunner {
+public class BackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "<connection string uri>";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
     }
 }
